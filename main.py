b2ba7b53ddaf87291eaa7d805317ce4c6ddd4701
lollocenne <lorenzo.cennerilli@gmail.com>
2024-11-16 17:15:38 +0100

    Added bold and size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-from PyQt5.QtWidgets import QMainWindow, QTextEdit
-
+from PyQt5.QtWidgets import QMainWindow, QTextEdit, QToolBar, QAction, QSpinBox, QLabel
+from PyQt5.QtGui import QFont, QTextCharFormat
 
 class TextEditor(QMainWindow):
     def __init__(self):
@@ -11,7 +10,7 @@ class TextEditor(QMainWindow):
         self.setGeometry(500, 200, 700, 600)
         self.showMaximized()
         
-        # Create the part where the text will be
+        # Create the text editing area
         self.textContainer = QTextEdit(self)
         self.setCentralWidget(self.textContainer)
         
@@ -19,9 +18,50 @@ class TextEditor(QMainWindow):
         font = self.textContainer.font()
         font.setPointSize(14)
         self.textContainer.setFont(font)
-
+        
+        # Current formatting
+        self.currentBold: bool = False
+        self.currentFontSize: int = 14
+        
+        # Create the toolbar
+        self.createToolbar()
+    
+    def createToolbar(self):
+        # Create a toolbar
+        toolbar = QToolBar("Tools", self)
+        self.addToolBar(toolbar)
+        
+        # Set the current formatting for the new text
+        def applyCurrentFormatting():
+            charFormat = QTextCharFormat()
+            charFormat.setFontWeight(QFont.Bold if self.currentBold else QFont.Normal)
+            charFormat.setFontPointSize(self.currentFontSize)
+            self.textContainer.setCurrentCharFormat(charFormat)
+        
+        def toggleBold(checked):
+            self.currentBold = checked
+            applyCurrentFormatting()
+        
+        def changeSize(size):
+            self.currentFontSize = size
+            applyCurrentFormatting()
+        
+        # Bold button
+        boldAction = QAction("Bold", self)
+        boldAction.setCheckable(True)
+        boldAction.triggered.connect(toggleBold)
+        toolbar.addAction(boldAction)
+        
+        # Font size button
+        toolbar.addWidget(QLabel("Size"))
+        self.fontSizeSpinbox = QSpinBox(self)
+        self.fontSizeSpinbox.setRange(1, 99)
+        self.fontSizeSpinbox.setValue(self.currentFontSize)
+        self.fontSizeSpinbox.valueChanged.connect(changeSize)
+        toolbar.addWidget(self.fontSizeSpinbox)
 
 if __name__ == "__main__":
+    import sys
     from PyQt5.QtWidgets import QApplication
     
     app = QApplication(sys.argv)
